75f0918ff739d5a2302ad69d566c4f4a79846695
Atsushi Ono <a.ono0331@gmail.com>
2013-03-25 00:39:59 +0900

    initial import of com.ibm.xsp.extlib.controls.ja

---
+++ com.ibm.xsp.extlib.controls.ja/resources/web/extlib/dijit/nls/ja/pickers.js
@@ -0,0 +1,19 @@
+{
+PickerCheckBox_OK: "OK",
+PickerCheckBox_Cancel: "キャンセル",
+PickerList_OK: "OK",
+PickerList_Cancel: "キャンセル",
+PickerListSearch_Search:"検索",
+PickerListSearch_OK: "OK",
+PickerListSearch_Cancel:"キャンセル",
+PickerListSearch_Value: "値",
+PickerListSearch_Values: "値",
+PickerName_Search:"検索",
+PickerName_Add:"追加",
+PickerName_Remove:"削除",
+PickerName_RemoveAll:"すべて削除",
+PickerName_OK:"OK",
+PickerName_Cancel:"キャンセル",
+PickerName_SearchIn:"検索場所",
+PickerName_SearchFor:"検索対象"
+}
